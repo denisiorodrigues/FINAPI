ce0328d1db82fb6a1a13cbeadd5ecbc761bf8672
= <=>
2021-11-14 16:31:49 -0300

    Deletar uma conta e retornar um balance

--- src/index.js
+++ src/index.js
@@ -127,4 +127,26 @@ app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
   return response.json(customer);
 });
 
+app.get("/accounts", (request, response) => {
+  return response.json(customers);
+});
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  customers.splice(customer,1);
+
+  return response.status(200).json(customers);
+
+});
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  return response.json(balance);
+
+});
+
 app.listen(3333);
