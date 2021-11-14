2016f277b150dfc50707c37fd1dd2fc2a2048a03
= <=>
2021-11-14 15:31:03 -0300

    Atualizar conta do cliente

--- src/index.js
+++ src/index.js
@@ -108,9 +108,23 @@ app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
   const statement = customer.statement.filter((statement) => statement.created_at.toDateString() ===
   new Date(dateFormat).toDateString());
 
-  console.log(new Date(dateFormat).toDateString());
-
   return response.json(statement);
+});
+
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
+
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
 });
 
 app.listen(3333);
